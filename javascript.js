6df26738b1d9fb3b01419653f73f24f1f323312d
Joshua Foate <joshuafoate@gmail.com>
2024-02-04 23:42:31 -0600

    Finished.

--- javascript.js
+++ javascript.js
@@ -1,11 +1,26 @@
-let count = 1;
+let count = 0;
 let buttonWidth = 100;
 let buttonHeight = 50;
+
 const noButton = document.querySelector('#no-button');
+const catGif = document.querySelector('#cat-gif');
 noButton.addEventListener('click', () => {
     count++;
-    buttonWidth += count * buttonWidth;
-    buttonHeight += count * buttonHeight;
+    buttonWidth += count + buttonWidth;
+    buttonHeight += count + buttonHeight;
     document.getElementById('yes-button').style.minWidth = `${buttonWidth}px`;
-    document.getElementById('yes-button').style.minWidth = `${buttonHeight}px`;
+    document.getElementById('yes-button').style.minHeight = `${buttonHeight}px`;
+    document.getElementById('cat-gif').src = "./img/goma-sad.gif";
+    document.getElementById('under-gif-text').innerText = "stop playing games :(";
+});
+
+const yesButton = document.querySelector('#yes-button');
+yesButton.addEventListener('click', () => {
+    document.getElementById('yes-button').style.minWidth = `100px`;
+    document.getElementById('yes-button').style.minHeight = `50px`;
+    buttonWidth = 100;
+    buttonHeight = 50;
+    count = 0;
+    document.getElementById('cat-gif').src = "./img/valentines-love.gif";
+    document.getElementById('under-gif-text').innerText = "YAY!";
 });
